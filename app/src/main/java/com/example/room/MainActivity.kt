8881cc2a7aab35a73692a90915f5c1ef7aee9fee
barkotenicholas <barkotenicholas@gmail.com>
2022-04-06 11:11:40 +0300

    changed the label arrtibute

--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
     }
 }
